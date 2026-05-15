f3beb7b88f8b1725c5e41bb5c74f70c434f0eec2
Mo Mohamud <mo.mohamud01@student.wandw.ac.nz>
2026-05-15 13:08:51 +1200

    Detail page Slide 8

--- SD7501Bulky.DataAccess/Repository/UnitOfWork.cs
+++ SD7501Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -14,13 +14,13 @@ namespace SD7501Bulky.DataAccess.Repository
     private readonly ApplicationDbContext _db;
 
     public ICategoryRepository Category { get; private set; }
-    public IProductRepository Product { get; private set; }
+    public IProductRepository Products { get; private set; }
 
     public UnitOfWork(ApplicationDbContext db)
     {
         _db = db;
         Category= new CategoryRepository(_db);
-        Product = new ProductRepository(_db);
+        Products = new ProductRepository(_db);
     }
 
     public void Save()
